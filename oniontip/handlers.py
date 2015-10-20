ed99a14ed8eb43626647e59afb6d8eb3aa3250e7
Finn <finn@finn.io>
2015-10-20 12:41:00 -0700

    Cleanup style with handlers.py

--- oniontip/handlers.py
+++ oniontip/handlers.py
@@ -12,7 +12,6 @@ class TlsSMTPHandler(SMTPHandler):
         """
         try:
             import smtplib
-            import string # for tls add this line
             try:
                 from email.utils import formatdate
             except ImportError:
@@ -23,14 +22,14 @@ class TlsSMTPHandler(SMTPHandler):
             smtp = smtplib.SMTP(self.mailhost, port)
             msg = self.format(record)
             msg = "From: %s\r\nTo: %s\r\nSubject: %s\r\nDate: %s\r\n\r\n%s" % (
-                            self.fromaddr,
-                            string.join(self.toaddrs, ","),
-                            self.getSubject(record),
-                            formatdate(), msg)
+                self.fromaddr,
+                ",".join(self.toaddrs, ","),
+                self.getSubject(record),
+                formatdate(), msg)
             if self.username:
-                smtp.ehlo() # for tls add this line
-                smtp.starttls() # for tls add this line
-                smtp.ehlo() # for tls add this line
+                smtp.ehlo()  # for tls add this line
+                smtp.starttls()  # for tls add this line
+                smtp.ehlo()  # for tls add this line
                 smtp.login(self.username, self.password)
             smtp.sendmail(self.fromaddr, self.toaddrs, msg)
             smtp.quit()
